16940577daf565192c8e5d2b563f3b3333647b83
sijamini <sijamini.b@hotmail.ch>
2021-05-07 02:04:17 +0200

    updates

--- PyGazeAnalyser-master/pygazeanalyser/pygazeanalyser_functions.py
+++ PyGazeAnalyser-master/pygazeanalyser/pygazeanalyser_functions.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append('PyGazeAnalyser-master\\pygazeanalyser')
 
 #pygaze imports
-from pygaze import *
+#from pygaze import *
 from edfreader import *
 from gazeplotter import *
 
@@ -46,6 +46,36 @@ def read_data(partic_id,game_nr,phase='stimulus'):
         start_message = 'CC'
         stop_message = 'TRIALEND'
     data = read_edf(fname,start=start_message,stop=stop_message) #start and stop indicators can be found in the ASC file
+    return data
+
+def read_data2(partic_id, phase='stimulus'):
+
+    """
+    Reads data from the asc file for different phases namely fixation, stimulus, reward
+
+    @Input: 
+	    Partic_id : Pariticipant id
+	    Phase     : 'fixation', 'stimulus' or 'reward'
+    
+
+    Returns:
+	   data : dict containing all the data from all games 	
+    """
+    if phase =='stimulus':
+        start_message = 'SYNCTIME'
+        stop_message = 'CC'
+    elif phase =='fixation':
+        start_message = 'TRIALID'
+        stop_message = 'SYNCTIME'
+    elif phase =='reward':
+        start_message = 'CC'
+        stop_message = 'TRIALEND'
+        
+    data = []
+    for game_nr in (1,16):
+        fname = 'Participant Data/'+str(partic_id)+ '/' + str(partic_id) + \
+                 str('_') + str(game_nr) + str('.asc') #path of the ASC file
+        data = data + read_edf(fname,start=start_message,stop=stop_message) #start and stop indicators can be found in the ASC file
     return data
 
 def load_dataset_properties(fname):
--- pygazeanalyser_functions.py
+++ pygazeanalyser_functions.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append('PyGazeAnalyser-master\\pygazeanalyser')
 
 #pygaze imports
-from pygaze import *
+#from pygaze import *
 from edfreader import *
 from gazeplotter import *
 
@@ -46,6 +46,36 @@ def read_data(partic_id,game_nr,phase='stimulus'):
         start_message = 'CC'
         stop_message = 'TRIALEND'
     data = read_edf(fname,start=start_message,stop=stop_message) #start and stop indicators can be found in the ASC file
+    return data
+
+def read_data2(partic_id, phase='stimulus'):
+
+    """
+    Reads data from the asc file for different phases namely fixation, stimulus, reward
+
+    @Input: 
+	    Partic_id : Pariticipant id
+	    Phase     : 'fixation', 'stimulus' or 'reward'
+    
+
+    Returns:
+	   data : dict containing all the data from all games 	
+    """
+    if phase =='stimulus':
+        start_message = 'SYNCTIME'
+        stop_message = 'CC'
+    elif phase =='fixation':
+        start_message = 'TRIALID'
+        stop_message = 'SYNCTIME'
+    elif phase =='reward':
+        start_message = 'CC'
+        stop_message = 'TRIALEND'
+        
+    data = []
+    for game_nr in (1,16):
+        fname = 'Participant Data/'+str(partic_id)+ '/' + str(partic_id) + \
+                 str('_') + str(game_nr) + str('.asc') #path of the ASC file
+        data = data + read_edf(fname,start=start_message,stop=stop_message) #start and stop indicators can be found in the ASC file
     return data
 
 def load_dataset_properties(fname):
@@ -157,8 +187,10 @@ def draw_heatmap_trial(data,partic_id,game_nr,trial_nr):
 	# draw saccadic scanpath and save fig
 	#saves as id+game_nr+trial_nr+current date time
 
-	savefilename = 'output_images/' + str(partic_id) + '_game_'+str(game_nr)+'_' \
-               + str(trial_nr) + '_'+ time.strftime("%Y%m%d-%H%M%S")
+	#savefilename = 'output_images/' + str(partic_id) + '_game_'+str(game_nr)+'_' \
+    #           + str(trial_nr) + '_'+ time.strftime("%Y%m%d-%H%M%S")
+    
+	savefilename = 'output_images/' + str(partic_id) + '_'+str(game_nr)+'_' + str(trial_nr)
 
 	#draw heatmap 
 	fig_heatmap = draw_heatmap(fixations, dispsize, imagefile=None)
@@ -237,5 +269,3 @@ def return_cross_pos(partic_id,game_nr,trial_nr):
         x_mean,y_mean = Efix[-2:]
 
     return x_mean, y_mean
-
-
