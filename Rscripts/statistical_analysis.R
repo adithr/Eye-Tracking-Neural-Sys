ebfbfa79f4dd73ecae46f5bd26b24f6751d606a8
sijamini <sijamini.b@hotmail.ch>
2021-05-30 19:48:27 +0200

    updated the main csv and the r script with the noise data

--- Rscripts/statistical_analysis.R
+++ Rscripts/statistical_analysis.R
@@ -5,6 +5,7 @@ rm(list=ls())
 # Clear console
 cat("\014")
 
+
 library(ggplot2)
 library(ez)
 library(multcomp)
@@ -13,6 +14,7 @@ library(pastecs)
 library(reshape)
 library(WRS2)
 library(plyr)
+library(ggstatsplot)
 
 # set working directory to data file directory:
 setwd("C:/Users/sijam/Documents/GitHub/Eye-Tracking-Neural-Sys/Data Files")
@@ -21,11 +23,17 @@ setwd("C:/Users/sijam/Documents/GitHub/Eye-Tracking-Neural-Sys/Data Files")
 dataOriginal<-read.csv("dataset_0423.csv", header = TRUE)
 data<-read.csv("results_data_subjects_V2.csv", header = TRUE)
 
-subsetOriginal <- dataOriginal[c(8065:10080),]
+data$velocity_low <- sapply(data$velocity_low, as.numeric)
+data$velocity_high <- sapply(data$velocity_high, as.numeric)
+data$delta_velocity <- sapply(data$delta_velocity, as.numeric)
+
+#subsetOriginal <- dataOriginal[c(8065:10080),]
 
 ###########
 # PLOTS
 ###########
+#hypothesis 1: radius in the heatmap during stimulation phase
+#subj_id vs delta radius
 bar <- ggplot(data, aes(subject_id, delta_radius_stimulation))
 bar + stat_summary(fun = mean, geom = "bar", fill = "White", colour = "Black") + stat_summary(fun.data = mean_cl_normal, geom = "pointrange") + labs(x = "subject_id", y = "delta_radius_stimulation")
 
@@ -36,14 +44,97 @@ scatter <- ggplot(data, aes(subject_id, delta_radius_stimulation))
 scatter + stat_summary(fun = mean, geom = "point") + labs(x = "subject_id", y = "delta_radius_stimulation")
 scatter + geom_point() + geom_smooth() + labs(x = "subject_id", y = "delta radius")
 
-#x = tapply(newData2$radii, newData2$subject_id + newData2$game_nr, mean) #merging data?not sure why I did this xD
-#x
+#high noise radius vs low noise radius
+#fancy line plots
+line2 <- ggplot(data, aes(radius_high_stimulation, radius_low_stimulation))
+line2 +   theme_grey(base_size = 16) + # get nice theme going
+  stat_summary(fun = mean, geom = "line", size = 1, aes(group=1), colour = "#FF0000") + # Add red lines to connect the group means
+  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", width = 0.2, size = 0.5, colour = "#FF0000") + # Add bootstrapped CI error bars
+  stat_summary(fun = mean, geom = "point", size = 4, colour = "#FF0000") + # Add large red dots to show mean radius for low noise for each radius for high noise level
+  stat_summary(fun = mean, geom = "point", size = 2, colour = "#FFFFFF") + # Add smaller white dots to fill the larger red dots
+  scale_x_discrete(labels=c("Placebo", "Low", "High")) + # add x tick labels
+  labs(x = "radius_high_stimulation", y = "radius_low_stimulation") + # add axis labels
+  ggtitle("low noise (means with bootstrapped 95% CIs) at different high doses") + # add a plot title
+  theme(plot.title = element_text(hjust = 0.5, face = "italic")) # center and italicize plot title
+
+scatter2 <- ggplot(data, aes(radius_high_stimulation, radius_low_stimulation))
+scatter2 + stat_summary(fun = mean, geom = "point") + labs(x = "subject_id", y = "delta_radius_stimulation")
+scatter2 + geom_point() + geom_smooth() + labs(x = "subject_id", y = "delta radius")
+#there seems to be a linear correlation between low noise and high noise stimulation phase :P
+
+#finally an useful plot, distinguishing between low_noise and high_noise
+#create new df with all radii in one column and their levels on the other column
+noise <- gl(2, 35, labels = c("high", "low"))
+radius <- c(data$radius_high_stimulation, data$radius_low_stimulation)
+dataRadius <-data.frame(noise, radius)
+names(dataRadius) <- c("noise","radius")
+
+boxplot <- ggplot(dataRadius, aes(noise, radius))
+boxplot + geom_boxplot() + labs(x = "noise", y ="radius")
+
+#hypothesis 2: velocity vs noise
+noise <- gl(2, 35, labels = c("high", "low"))
+velocity <- c(data$velocity_high, data$velocity_low)
+dataVelocity <-data.frame(noise, velocity)
+
+boxplot <- ggplot(dataVelocity, aes(noise, velocity))
+boxplot + geom_boxplot() + labs(x = "noise", y ="velocity") #outliers!!!!
+
+#tag outliers
+boxplot(dataVelocity)$out
+ggbetweenstats(dataVelocity, noise, velocity, outlier.tagging = TRUE)
+
+#remove outliers
+qLow <- quantile(data$velocity_low, probs=c(.25, .75), na.rm = TRUE)
+qHigh <- quantile(data$velocity_high, probs=c(.25, .75), na.rm = TRUE)
+iqrLow <- IQR(data$velocity_low, na.rm = TRUE)
+iqrHigh <- IQR(data$velocity_high, na.rm = TRUE)
+upLow <-  qLow[2]+1.5*iqrLow # Upper Range  
+lowLow<- qLow[1]-1.5*iqrLow # Lower Range
+upHigh <-  qHigh[2]+1.5*iqrHigh # Upper Range  
+lowHigh<- qHigh[1]-1.5*iqrHigh # Lower Range
+velocity_low_withoutOut <- subset(data, data$velocity_low > (qLow[1] - 1.5*iqrLow) & data$velocity_low < (qLow[2]+1.5*iqrLow))
+#velocity_low_withoutOut <- subset(data, data$velocity_low > (qLow[1] - 1.5*iqrLow) & data$velocity_low < 0.003406)
+velocity_high_withoutOut <- subset(data, data$velocity_high > (qHigh[1] - 1.5*iqrHigh) & data$velocity_high < (qHigh[2]+1.5*iqrHigh))
+#velocity_high_withoutOut <- subset(data, data$velocity_high > (qHigh[1] - 1.5*iqrHigh) & data$velocity_high < 0.004438)
+data_withoutOut <- subset(velocity_low_withoutOut, velocity_low_withoutOut$velocity_high > (qHigh[1] - 1.5*iqrHigh) & velocity_low_withoutOut$velocity_high < (qHigh[2]+1.5*iqrHigh))
+
+#new data & new boxplot
+noise <- c("high","high","high","high","high","high","high","high","high","high",
+          "high","high","high","high","high","high","high","high","high","high",
+          "high","high","high","high","high","high", "high","high",
+          "low","low","low","low","low","low","low","low","low","low","low",
+          "low","low","low","low","low","low","low","low","low","low",
+          "low","low","low")
+noise <-factor(noise, levels = c("high", "low"))
+velocity <- c(velocity_high_withoutOut$velocity_high, velocity_low_withoutOut$velocity_low)
+dataVelocity <-data.frame(noise, velocity)
+
+boxplot <- ggplot(dataVelocity, aes(noise, velocity))
+boxplot + geom_boxplot(outlier.shape=NA) + labs(x = "noise", y ="velocity")+
+  scale_y_continuous(limits = c(0,0.0025))
+
+
+boxplot(dataVelocity, outline=F)$out
+ggbetweenstats(dataVelocity, noise, velocity, outlier.tagging = TRUE, outline=F)
 
 ##############################
 # REPEATED MEASURES ERROR BARS 
 ##############################
 # Load user-defined function file 'rmMeanAdjust.R' into R's global environment:
 source("C:/Users/sijam/Documents/GitHub/Eye-Tracking-Neural-Sys/Rscripts/rmMeanAdjust.R")
+
+#plot our bar graphs with RM error bars:
+spiderBar <- ggplot(dataRadius, aes(noise, radius)) # create plot of anxiety in the two levels of our factor
+spiderBar + 
+  theme_grey(base_size = 14) + # get nice theme going
+  stat_summary(fun = mean, geom = "bar", fill = "White", colour = "Black") + # add the bars
+  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.2) + # add the error bars
+  labs(x = "Type of Noise", y = "Radius") + # label the axes
+  ggtitle("Data with correct error bars") + # add a plot title
+  theme(plot.title = element_text(hjust = 0.5, face = "italic")) + # center and italicize plot title
+  scale_x_discrete(labels=c("high noise", "low noise")) + # add bar labels
+  scale_y_continuous(limits = c(0, 20), breaks = seq(from = 0, to = 20, by = 10)) # adapt y-scale 
 
 data$Factor = factor("Delta") # Factor with only one level: Delta
 
@@ -74,7 +165,7 @@ data<-read.csv("results_data_subjects_V2.csv", header = TRUE)
 #hypothesis 1:radius in the heatmap during stimulation phase
 stat.desc(data$delta_radius_stimulation, basic = FALSE, norm = TRUE) # Using the 'delta' column 
 #hypothesis 2:
-
+stat.desc(data$delta_velocity, basic = FALSE, norm = TRUE) # Using the 'delta' column 
 #hypothesis 3:
 
 
@@ -83,7 +174,11 @@ stat.desc(data$delta_radius_stimulation, basic = FALSE, norm = TRUE) # Using the
 dep.t.test <- t.test(data$radius_high_stimulation,data$radius_low_stimulation, paired = TRUE)
 dep.t.test
 #hypothesis 2:
-
+dep.t.test <- t.test(data$velocity_low,data$velocity_high, paired = TRUE)
+dep.t.test
+#without outliers:
+dep.t.test <- t.test(data_withoutOut$velocity_low, data_withoutOut$velocity_high, paired = TRUE)
+dep.t.test
 #hypothesis 3:
 
 
